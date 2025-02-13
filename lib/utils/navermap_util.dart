a17823ac0377d7619ca800741a87aa08bb75f68d
EonEon2 <duqdjs123@naver.com>
2025-02-13 17:45:25 +0900

    chore: marker list

--- lib/utils/navermap_util.dart
+++ lib/utils/navermap_util.dart
@@ -3,12 +3,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 
-
 final String? naver_client_id = dotenv.env['NAVER_CLIENT_ID'];
 
 class navermap_util {
   static Completer<NaverMapController> _controller = Completer();
-
+  static List<NMarker> markers = []; // 마커 리스트로 상태 관리
 
   // NaverMap SDK 초기화
   static Future<void> initializeNaverMap() async {
@@ -20,6 +19,12 @@ class navermap_util {
   static void onMapCreated(NaverMapController controller) {
     if (_controller.isCompleted) _controller = Completer();
     _controller.complete(controller);
+    print("Map created and controller initialized.");
+
+    // 이미 추가된 마커가 있다면, 다시 지도에 추가
+    markers.forEach((marker) async {
+      await controller.addOverlay(marker);
+    });
   }
 
   // NaverMapController 반환
@@ -28,17 +33,20 @@ class navermap_util {
   }
 
   static Future<void> addMarker(double wlati, double wlong, String jpname) async {
-    print("addmarker -------");
-    print(wlati);
     NaverMapController controller = await getController();
 
+    // 새로운 마커 생성
     final marker = NMarker(
       id: '$jpname-${DateTime.now().millisecondsSinceEpoch}',
       position: NLatLng(wlati, wlong),
     );
-    print(marker);
-    await controller.addOverlay(marker);
 
+    // 마커를 리스트에 추가
+    markers.add(marker);
+
+    // 지도에 마커 추가
+    await controller.addOverlay(marker);
+    print("Added marker at $wlati, $wlong with ID: ${marker}");
   }
 
   static Widget buildNaverMap() {
@@ -49,7 +57,7 @@ class navermap_util {
           zoom: 15.0, // 초기 확대 비율
         ),
       ),
-      onMapReady: onMapCreated,
+      onMapReady: onMapCreated, // onMapCreated 콜백 추가
     );
   }
 }
